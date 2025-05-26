993bc5c9c6074f676945267da751a386315457f5
Gen351 <qw040221@gmail.com>
2025-05-26 11:48:14 +0800

    added the URL and KEY back

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
-// const SUPABASE_URL = 'https://qvauvxbijwitxaqcbpoy.supabase.co';
-// const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2YXV2eGJpandpdHhhcWNicG95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1NzQ4NjIsImV4cCI6MjA2MTE1MDg2Mn0.PF2c4VZxwH1lRQ0bHh4mGKY3r-AFz5dpLD79Egf4ZIo';
+const SUPABASE_URL = 'https://qvauvxbijwitxaqcbpoy.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2YXV2eGJpandpdHhhcWNicG95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1NzQ4NjIsImV4cCI6MjA2MTE1MDg2Mn0.PF2c4VZxwH1lRQ0bHh4mGKY3r-AFz5dpLD79Egf4ZIo';
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
     console.warn("Supabase URL or Anon Key is missing. Please update js/supabaseClient.js");
